2de05264f78c1fd5942689c748ca33531c522159
gaodawei <dwgao@alauda.io>
2019-08-21 16:19:24 +0800

    add to chart

--- src/main/java/demo/ms/message/MessageServiceApplication.java
+++ src/main/java/demo/ms/message/MessageServiceApplication.java
@@ -3,9 +3,13 @@ package demo.ms.message;
 import demo.ms.common.config.ResourceServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Import;
 
+@EnableHystrix
+@EnableCircuitBreaker
 @EnableEurekaClient
 @Import(ResourceServerConfig.class)
 @SpringBootApplication
